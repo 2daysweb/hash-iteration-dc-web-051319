fd9e8395f0508ce50e03eff48ec92ea3ec9bc900
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-26 22:32:59 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,6 +7,10 @@
 # }
 
 def select_winner(passengers)
+<<<<<<< HEAD
+=======
+  winner = ""
+>>>>>>> 3e78e74af854cb3ab29b8a2036eb07800145973b
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
@@ -14,5 +18,14 @@ passengers.each do |suite, name|
   end
 end
  
+<<<<<<< HEAD
 winner
 end 
+=======
+winner      winner = name 
+end
+end 
+
+winner
+end
+>>>>>>> 3e78e74af854cb3ab29b8a2036eb07800145973b
